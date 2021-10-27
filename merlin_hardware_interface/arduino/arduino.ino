2e708069b0737f402c5f6df7622ec96af5df971b
Nile Walker <nilezwalker@gmail.com>
2021-10-27 12:33:45 -0400

    Added Proposal

--- merlin_hardware_interface/arduino/arduino.ino
+++ merlin_hardware_interface/arduino/arduino.ino
@@ -1,31 +1,39 @@
 #define WAIST_PULSE_PIN 7
 #define WAIST_DIR_PIN 6
 #define WAIST_BRAKE_PIN 8
+#define WAIST_SPR 200
+#define WAIST_REDUCTION 48 // 1:x
 
 #define SHOULDER_PULSE_PIN 5
 #define SHOULDER_DIR_PIN 4
-#define SHOULDER_BRAKE_PIN 7
+#define SHOULDER_BRAKE_PIN 9
+#define SHOULDER_SPR 200
+#define SHOULDER_REDUCTION 48 // 1:x
 
 #define ELBOW_PULSE_PIN 3
 #define ELBOW_DIR_PIN 2
 #define ELBOW_BRAKE_PIN 10
+#define ELBOW_SPR 200
+#define ELBOW_REDUCTION 48 // 1:x
 
-#define WRIST_AXIS_1_PULSE_PIN 14
-#define WRIST_AXIS_1_DIR_PIN 6
+#define AXIS_4_PULSE_PIN 11
+#define AXIS_4_DIR_PIN 12
 
-#define WRIST_AXIS_2_PULSE_PIN 15
-#define WRIST_AXIS_2_DIR_PIN 6
+#define AXIS_5_PULSE_PIN 13
+#define AXIS_5_DIR_PIN 14
 
-#define WRIST_AXIS_3_PULSE_PIN 15
-#define WRIST_AXIS_3_DIR_PIN 6
+#define AXIS_6_PULSE_PIN 15
+#define AXIS_6_DIR_PIN 16
 
 #define ONE_DEGREE 0.0175
+#define PI 3.14
 
-float waist_rps = 0.00065; // Radian Per Step
-
-float shoulder_rps = 0.00065; // Radian Per Step
-
-float elbow_rps = 0.00065; // Radian Per Step
+#define WAIST_RAD_PER_STEP (((2 * PI) / WAIST_SPR) / WAIST_REDUCTION)
+#define SHOULDER_RAD_PER_STEP (((2 * PI) / SHOULDER_SPR) / SHOULDER_REDUCTION)
+#define ELBOW_RAD_PER_STEP (((2 * PI) / ELBOW_SPR) / ELBOW_REDUCTION)
+#define WRIST_ROT_RAD_PER_STEP 0
+#define WRIST_FLEX_RAD_PER_STEP 0
+#define HAND_ROLL_RAD_PER_STEP 0
 
 union open_float
 {
@@ -33,21 +41,12 @@ union open_float
   float value = 0;
 };
 
-open_float waist;
-open_float shoulder;
-open_float elbow;
+open_float waist, shoulder, elbow, wrist_roll, wrist_flex, hand_roll;
+open_float waist_target, shoulder_target, elbow_target, wrist_roll_target, wrist_flex_target, hand_roll_target;
 
-open_float wrist_rotation;
-open_float wrist_flex;
-open_float hand_roll;
-
-open_float waist_target;
-open_float shoulder_target;
-open_float elbow_target;
-
-open_float wrist_rotation_target;
-open_float wrist_flex_target;
-open_float hand_roll_target;
+int pulse_pins[6] = {WAIST_PULSE_PIN, SHOULDER_PULSE_PIN, ELBOW_PULSE_PIN, AXIS_4_PULSE_PIN, AXIS_5_PULSE_PIN, AXIS_6_PULSE_PIN};
+int dir_pins[6] = {WAIST_DIR_PIN, SHOULDER_DIR_PIN, ELBOW_DIR_PIN, AXIS_4_DIR_PIN, AXIS_5_DIR_PIN, AXIS_6_DIR_PIN};
+bool wrist_first = false;
 
 void setup_pins()
 {
@@ -63,14 +62,14 @@ void setup_pins()
   pinMode(ELBOW_DIR_PIN, OUTPUT);
   pinMode(ELBOW_BRAKE_PIN, OUTPUT);
 
-  pinMode(WRIST_AXIS_1_PULSE_PIN, OUTPUT);
-  pinMode(WRIST_AXIS_1_DIR_PIN, OUTPUT);
+  pinMode(AXIS_4_PULSE_PIN, OUTPUT);
+  pinMode(AXIS_4_DIR_PIN, OUTPUT);
 
-  pinMode(WRIST_AXIS_2_PULSE_PIN, OUTPUT);
-  pinMode(WRIST_AXIS_2_DIR_PIN, OUTPUT);
+  pinMode(AXIS_5_PULSE_PIN, OUTPUT);
+  pinMode(AXIS_5_DIR_PIN, OUTPUT);
 
-  pinMode(WRIST_AXIS_3_PULSE_PIN, OUTPUT);
-  pinMode(WRIST_AXIS_3_DIR_PIN, OUTPUT);
+  pinMode(AXIS_6_PULSE_PIN, OUTPUT);
+  pinMode(AXIS_6_DIR_PIN, OUTPUT);
 }
 
 void load_saved_state()
@@ -78,63 +77,159 @@ void load_saved_state()
   waist.value = 0;
   shoulder.value = 0;
   elbow.value = 0;
-  wrist_rotation.value = 0;
+  wrist_roll.value = 0;
   wrist_flex.value = 0;
   hand_roll.value = 0;
 }
 
-void unlock_brakes(){
-
-}
-
-void lock_brakes(){
-
-}
-
-void setup_arm()
+void set_brakes(bool lock)
 {
-  setup_pins();
-  load_saved_state();
-  unlock_brakes();
+  digitalWrite(WAIST_BRAKE_PIN, lock ? HIGH : LOW);
+  digitalWrite(SHOULDER_BRAKE_PIN, lock ? HIGH : LOW);
+  digitalWrite(ELBOW_BRAKE_PIN, lock ? HIGH : LOW);
+  delay(500);
 }
 
 void setup()
 {
   Serial.begin(115200);
-  setup_arm();
+
+  setup_pins();
+
+  load_saved_state();
+
+  set_brakes(false);
+
+  Serial.println("MERLIN MR6500 Controller Started");
 }
 
-void update_arm()
+void update_arm_state()
 {
-  if (abs(waist.value - waist_target.value) > ONE_DEGREE / 2)
+  waist.value = ;
+  shoulder.value = ;
+  elbow.value = ;
+  wrist_roll.value = ;
+  wrist_flex.value = ;
+  hand_roll.value = ;
+}
+
+void update_arm_controls()
+{
+  int axis_steps[6] = {0, 0, 0, 0, 0, 0};
+
+  if (abs(waist.value - waist_target.value) > (ONE_DEGREE / 2))
   {
-    digitalWrite(WAIST_DIR_PIN, (waist.value < waist_target.value) ? LOW : HIGH);
-    waist.value += (waist.value < waist_target.value) ? waist_rps : -waist_rps;
-    digitalWrite(WAIST_PULSE_PIN, HIGH);
-    delay(6);
-    digitalWrite(WAIST_PULSE_PIN, LOW);
+    axis_steps[0] = (waist.value > waist_target.value) ? -1 : 1; // Flipped Axis
   }
-  if (abs(shoulder.value - shoulder_target.value) > ONE_DEGREE / 2)
+
+  if (abs(shoulder.value - shoulder_target.value) > (ONE_DEGREE / 2))
   {
-    digitalWrite(SHOULDER_DIR_PIN, (shoulder.value < shoulder_target.value) ? HIGH : LOW);
-    // Account for -1 Mechanical Reduction
-    elbow.value += (shoulder.value < shoulder_target.value) ? shoulder_rps : -shoulder_rps;
-    shoulder.value += (shoulder.value < shoulder_target.value) ? shoulder_rps : -shoulder_rps;
-    digitalWrite(SHOULDER_PULSE_PIN, HIGH);
-    delay(4);
-    digitalWrite(SHOULDER_PULSE_PIN, LOW);
+    axis_steps[1] = (shoulder.value > shoulder_target.value) ? 1 : -1;
   }
-  if (abs(elbow.value - elbow_target.value) > ONE_DEGREE / 2)
+
+  if (abs(elbow.value - elbow_target.value) > (ONE_DEGREE / 2))
   {
-    digitalWrite(ELBOW_DIR_PIN, (elbow.value < elbow_target.value) ? HIGH : LOW);
-    elbow.value += (elbow.value < elbow_target.value) ? elbow_rps : -elbow_rps;
-    digitalWrite(ELBOW_PULSE_PIN, HIGH);
-    delay(4);
-    digitalWrite(ELBOW_PULSE_PIN, LOW);
+    axis_steps[2] = (elbow.value > elbow_target.value) ? 1 : -1;
+  }
+  // Start Update Wrist
+  if (wrist_first)
+  {
+    if (abs(wrist_roll.value - wrist_roll_target.value) > (ONE_DEGREE / 2))
+    {
+      if (wrist_roll.value > wrist_roll_target.value)
+      {
+        axis_steps[3], axis_steps[4], axis_steps[5] = 1;
+      }
+      else
+      {
+        axis_steps[3], axis_steps[4], axis_steps[5] = -1;
+      }
+    }
+    if (abs(wrist_flex.value - wrist_flex_target.value) > (ONE_DEGREE / 2))
+    {
+      if ((wrist_flex.value > wrist_flex_target.value) and axis_steps[0] != 1)
+      {
+        axis_steps[4] += 1;
+        axis_steps[5] += 1;
+      }
+      else if ((wrist_flex.value < wrist_flex_target.value) and axis_steps[0] != -1)
+      {
+        axis_steps[4] -= 1;
+        axis_steps[5] -= 1;
+      }
+    }
+    if (abs(hand_roll.value - hand_roll_target.value) > (ONE_DEGREE / 2))
+    {
+      if ((hand_roll.value > hand_roll_target.value) and axis_steps[1] != 1)
+      {
+        axis_steps[5] += 1;
+      }
+      if ((hand_roll.value < hand_roll_target.value) and axis_steps[1] != -1)
+      {
+        axis_steps[5] -= 1;
+      }
+    }
+  }
+  else
+  {
+
+    if (abs(hand_roll.value - hand_roll_target.value) > (ONE_DEGREE / 2))
+    {
+      if ((hand_roll.value > hand_roll_target.value) and axis_steps[1] != 1)
+      {
+        axis_steps[5] += 1;
+      }
+      if ((hand_roll.value < hand_roll_target.value) and axis_steps[1] != -1)
+      {
+        axis_steps[5] -= 1;
+      }
+    }
+    if (abs(wrist_flex.value - wrist_flex_target.value) > (ONE_DEGREE / 2))
+    {
+      if ((wrist_flex.value > wrist_flex_target.value) and axis_steps[0] != 1)
+      {
+        axis_steps[4] += 1;
+        axis_steps[5] += 1;
+      }
+      else if ((wrist_flex.value < wrist_flex_target.value) and axis_steps[0] != -1)
+      {
+        axis_steps[4] -= 1;
+        axis_steps[5] -= 1;
+      }
+    }
+
+    if (abs(wrist_roll.value - wrist_roll_target.value) > (ONE_DEGREE / 2))
+    {
+      if (wrist_roll.value > wrist_roll_target.value)
+      {
+        axis_steps[3], axis_steps[4], axis_steps[5] = 1;
+      }
+      else
+      {
+        axis_steps[3], axis_steps[4], axis_steps[5] = -1;
+      }
+    }
+  }
+  wrist_first = !wrist_first; // Swap ordering each time step
+  // Finished Update Wrist //
+
+  // Minimize delay by stepping all motors at once
+  for (int i = 0; i < 6; i++)
+  {
+    if (axis_steps[i] != 0)
+    {
+      digitalWrite(dir_pins[i], (axis_steps[i] > 0) ? HIGH : LOW);
+      digitalWrite(pulse_pins[i], HIGH);
+    }
+  }
+  delay(4);
+  for (int i = 0; i < 6; i++)
+  {
+    digitalWrite(pulse_pins[i], LOW);
   }
 }
 
-void loop()
+void handle_commands()
 {
   if (Serial.available() > 0)
   {
@@ -144,7 +239,7 @@ void loop()
       Serial.write(waist.bytes, 4);
       Serial.write(shoulder.bytes, 4);
       Serial.write(elbow.bytes, 4);
-      Serial.write(wrist_rotation.bytes, 4);
+      Serial.write(wrist_roll.bytes, 4);
       Serial.write(wrist_flex.bytes, 4);
       Serial.write(hand_roll.bytes, 4);
     }
@@ -153,7 +248,7 @@ void loop()
       Serial.readBytes(waist_target.bytes, 4);
       Serial.readBytes(shoulder_target.bytes, 4);
       Serial.readBytes(elbow_target.bytes, 4);
-      Serial.readBytes(wrist_rotation.bytes, 4);
+      Serial.readBytes(wrist_roll.bytes, 4);
       Serial.readBytes(wrist_flex.bytes, 4);
       Serial.readBytes(hand_roll.bytes, 4);
     }
@@ -162,20 +257,24 @@ void loop()
       Serial.println(waist.value, 4);
       Serial.println(shoulder.value, 4);
       Serial.println(elbow.value, 4);
-      Serial.println(wrist_rotation.value, 4);
+      Serial.println(wrist_roll.value, 4);
       Serial.println(wrist_flex.value, 4);
       Serial.println(hand_roll.value, 4);
     }
     if (cmd == 'T')
     {
-      shoulder_target.value = 1.571;
-      elbow_target.value = 1.571;
+      wrist_roll_target.value = 1.5708;
     }
     if (cmd == 'Z')
     {
-      shoulder_target.value = 0;
-      elbow_target.value = 0;
+      wrist_roll_target.value = 0;
     }
   }
-  update_arm();
+}
+
+void loop()
+{
+  handle_commands();
+  update_arm_controls();
+  update_arm_state();
 }
---
+++ status_updates/proposal/proposal.md
@@ -0,0 +1,34 @@
+# Enabling Robotic Automation at Staunton Makerspace
+
+Robotic machining can be highly flexible because robots can be adjusted for a variety of machining processes by changing the end-effectors or tools attached to the manipulator. As robotic machining systems have more DOF than machine tools, more complex parts can be machined. Since work on the Merlin MR6500 Arm in the shop has gone well its become a realistic capability if we want to pursue it. 
+## Potential Uses:
+This arm is hefty and theres almost nothing you couldn't stick to it. So it's limitations are basically
+At this point it's limitations
+* Large Scale 3D Printing 
+* Photogrammetry
+* Automated 
+  * Welding
+  * Machining 
+  * Painting
+## Current Abilities
+### Degrees of Freedom
+Right now we have control of the three major axes using 3 of the 12 V stepper drivers that I had on hand. These 3 axes are waist rotation, shoulder rotation and elbow rotation.
+### Offline Programming
+Using ROS + MoveIt, it's possible to set goal states in the GUI by dragging a virtual arm or sending cartesian positions to the planner through something like a python script. The arm will then approach this target position while checking to see if it's going to hit itself. ```NOTE: The simulator is set up with all 6DOF despite only 3 working```
+## Current Limitations
+### Degrees of Freedom
+Right now we have control of the three major axes using 3 of the 12 V stepper drivers that I had on hand. These 3 axes are waist rotation, shoulder rotation and elbow rotation.
+### Offline Programming
+ROS is very useful but it's meant for development and not a user facing tool. Setting up new tool attachments is an involved process and documentation on how to do so is limited and oriented more towards those already familiar with ROS. Also there's no standardized way to communicate between ROS and CAM. So to actually allow this to be accessible as a tool we would need to buy a license to dedicated robot programming software. Which is at this point the most expensive part.
+
+## Current Plan
+  1. Upgrade the power supply to a 48V Supply that can better leverage the capabilities of the motors. 
+  2. Add stepper drivers for the 3 wrist axes.
+  3. Switch from counting steps to reading from the joint encoders to improve accuracy and reliability. Plus I'd like to add limit switches for homing the arm.
+  4. Reinstall the brakes for the 3 major axis.
+  5. Set up an enclosure to better manage the power supply, emergency shutoff, stepper drivers and cabling. 
+  6. At this stage we should have a fully working and cleaned up arm so I plan to go back into ROS and set up some milling tests with basic shapes and python since I don't have proper CAM Software.
+  7.  I've already done this for RoboDK but once a cam software is decided on I'll update the models to fit whatever library it uses. 
+  8.  As more of a bonus the arm did come with a pneumatic tool change system. In the long term it might be useful to get this up and running again so that jobs can be run with multiple tools. But I do think that actually utilizing the tool change feature would be complicated process to get setup that I wouldn't bother undertaking unless explicitly necessary.
+
+## [Parts List](https://docs.google.com/spreadsheets/d/1EjQkyqkRLtM95hAEB7xOX4P-yniZp_DnrhcI5OFt3fY/edit?usp=sharing)
