6784171e2ea9a8d9a4714c749d8fc4b859a2ca2f
Nile Walker <nilezwalker@gmail.com>
2022-01-21 11:23:41 -0500

    Updated Test

--- merlin_bringup/test.py
+++ merlin_bringup/test.py
@@ -5,6 +5,7 @@ from pilz_robot_programming import *
 import math
 import moveit_commander
 import rospy
+import numpy as np
 
 __REQUIRED_API_VERSION__ = "1"
 _DEFAULT_PLANNING_GROUP = "arm" 
@@ -18,18 +19,38 @@ print("Executing " + __file__)
 r = Robot(__REQUIRED_API_VERSION__)
 group_name = "arm"
 move_group = moveit_commander.MoveGroupCommander(group_name)
-current_pose = move_group.get_current_pose().pose
-                        
+
+cut_home = Ptp(goal=np.deg2rad([27,-19,-3,144,34,139]), vel_scale=0.05, acc_scale=0.05, planning_group="arm",target_link="Face_Plate_1",reference_frame="base_link")
+
+current_pose = move_group.get_current_pose().pose                        
 pose_goal = current_pose
+
 while True:
     sequence = Sequence()
-    moves = [(0,0,.20),(0,.2,0),(0,0,-0.2),(0,-.2,0)]
-
-    for x, y, z in moves:
-        r.move(Ptp(goal=Pose(position=Point(x, y, z)), relative=True,
-           vel_scale=0.1, acc_scale=0.1, planning_group="arm",target_link="Face_Plate_1",reference_frame="base_link"))
-        # sequence.append(Ptp(goal=pose_goal, planning_group="arm",target_link="Face_Plate_1",reference_frame="base_link"))
-
+    r.move(cut_home)
+    step_size = 0.0254
+    step_length = step_size
+    x, y = 0, 0
+    i = 0
+    for _ in range(5):
+        move = [0, 0, 0]
+        if i % 2 == 0:
+            if i > 1:
+                move[0] = step_length
+            else:
+                move[0] = -step_length
+        else:
+            if i > 1:
+                move[1] = step_length
+            else:
+                move[1] = -step_length
+        i += 1
+        i %= 4
+        step_length += step_size 
+        print(move)
+        cut = Lin(goal=Pose(position=Point(*move)), relative=True,
+            vel_scale=0.01, acc_scale=0.01, planning_group="arm",target_link="Face_Plate_1",reference_frame="base_link")
+        sequence.append(cut)
     print("Starting")
-    # r.move(sequence)
+    r.move(sequence)
     time.sleep(4)
